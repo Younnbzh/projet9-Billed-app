600b93cc9a5741acd8d63ca1c5e1685bbde5ecdc
Lionel LT <trocquer@gmail.com>
2025-01-29 14:51:19 +0100

    Bug 3 - seulement Jpg png

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -18,6 +18,14 @@ export default class NewBill {
   handleChangeFile = e => {
     e.preventDefault()
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    // Validate file type
+    const inputFile = this.document.querySelector(`input[data-testid="file"]`)
+    const allowedTypes = ['image/jpeg', 'image/png']
+    if (!allowedTypes.includes(file.type)) {
+        alert('Merci d\'uploader uniquement des fichiers JPG, JPEG, ou PNG')
+        inputFile.value = ''
+        return
+    }
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
     const formData = new FormData()
